d471affe10a730855e6a0e5c75e76fbe1a3eda79
cydoa11 <light.oa17@gmail.com>
2020-10-30 19:59:01 +0100

    smodify

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -110,7 +110,7 @@ class Category extends BaseModel
 	}
 
 	public static function getFieldId($catName){
-		$catId = Category::where(['name' => $catName])->get('id');
+		$catId = Category::where(['name' => $catName])->get('id')[0];
 		dd ($catId);
 	}
 	
