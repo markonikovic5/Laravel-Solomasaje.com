1f89537a7018dac53bf340e4c1f25763f860d3e6
cydoa11 <light.oa17@gmail.com>
2020-10-30 20:15:37 +0100

    catname

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -110,6 +110,7 @@ class Category extends BaseModel
 	}
 
 	public static function getFieldId($catName){
+		dd ($catName);
 		$catId = Category::where(['name' => $catName])->get('id')[0]->id;
 		dd ($catId);
 	}
