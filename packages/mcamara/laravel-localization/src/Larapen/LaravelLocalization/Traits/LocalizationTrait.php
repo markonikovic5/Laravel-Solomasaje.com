d1bff85e5e909a6e58406fc4465ff2707a2a0e64
cydoa11 <light.oa17@gmail.com>
2020-10-31 16:06:32 +0100

    routepath

--- packages/mcamara/laravel-localization/src/Larapen/LaravelLocalization/Traits/LocalizationTrait.php
+++ packages/mcamara/laravel-localization/src/Larapen/LaravelLocalization/Traits/LocalizationTrait.php
@@ -72,8 +72,7 @@ trait LocalizationTrait
 							'catSlug'     => $cat->slug,
 						], $locale);
 					}
-					dd ($localePath);
-					var_dump ($routePath);
+					dd ($routePath);
 					var_dump ($queryString); exit;
 					$url = app('url')->to($localePath . $routePath) . $queryString;
 				}
