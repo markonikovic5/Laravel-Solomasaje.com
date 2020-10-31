3b5063c2c79b041f5fa60fb1ce7f4e3eccf81493
cydoa11 <light.oa17@gmail.com>
2020-10-31 07:36:52 +0100

    trans

--- app/Helpers/UrlGen.php
+++ app/Helpers/UrlGen.php
@@ -171,7 +171,6 @@ class UrlGen
 					'subCatSlug'  => $entry->slug,
 					'city'        => 'city',
 				];
-				dd (trans('routes.v-search-subCat', $attr, $locale));
 				$url = lurl(trans('routes.v-search-subCat', $attr, $locale), $attr, $locale);
 			} else {
 				$url = '#';
