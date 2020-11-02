87e79d4ff313a5429e401e74f8252ceee09a25e2
cydoa11 <light.oa17@gmail.com>
2020-11-02 21:18:03 +0100

    tst-searchController

--- packages/mcamara/laravel-localization/src/Larapen/LaravelLocalization/LaravelLocalization.php
+++ packages/mcamara/laravel-localization/src/Larapen/LaravelLocalization/LaravelLocalization.php
@@ -298,7 +298,7 @@ class LaravelLocalization
 				$url = url($this->request->getRequestUri());
 			}
 		} else {
-			if($attributes['location']){
+			if(isset($attributes['location'])){
 				dd ("this is searchController");
 			}
 			// Get URL through entered Route (Or through entered URL)
