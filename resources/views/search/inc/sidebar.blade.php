097b87cb130d28b0234c28c70d551302d06c47ab
cydoa11 <light.oa17@gmail.com>
2020-11-02 14:35:07 +0100

    redefineurl

--- resources/views/search/inc/sidebar.blade.php
+++ resources/views/search/inc/sidebar.blade.php
@@ -165,12 +165,12 @@
 							<li>
 								@if ((isset($uriPathCityId) and $uriPathCityId == $city->id) or (request()->input('l')==$city->id))
 									<strong>
-										<a href="{{ route('masajes', ['cat' => $locationParams['c'], 'subCat' => $locationParams['sc'], 'cityName' => $locationParams['l']]) }}" title="{{ $city->name }}">
+										<a href="{{ route('masajes', ['catInfo' => $locationParams['c'], 'cityInfo' => $locationParams['l']]) }}" title="{{ $city->name }}">
 											{{ $city->name }}
 										</a>
 									</strong>
 								@else
-									<a href="{{ route('masajes', ['cat' => $locationParams['c'], 'subCat' => $locationParams['sc'], 'cityName' => $locationParams['l']]) }}" title="{{ $city->name }}">
+									<a href="{{ route('masajes', ['catInfo' => $locationParams['c'], 'cityInfo' => $locationParams['l']]) }}" title="{{ $city->name }}">
 										{{ $city->name }}
 									</a>
 								@endif
