6c14e09d071d267c20ba12074265cb50b5348ecd
cydoa11 <light.oa17@gmail.com>
2020-11-04 14:38:42 +0100

    old

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -34,36 +34,41 @@ class SearchController extends BaseController
 	/**
 	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
 	 */
-	public function index($catSlug, $location, $locationID)
+	public function index()
 	{
 		view()->share('isIndexSearch', $this->isIndexSearch);
 
 		// Pre-Search
-		$subCatSlug = Category::getCategory($catSlug);
-		if($subCatSlug->parent_id == 0){
-			$this->getCategory($subCatSlug->id);
-
-			// Get Category nested IDs
-			$catNestedIds = (object)[
-				'parentId' => 0,
-				'id'       => $subCatSlug->id,
-			];
-		} else {
-			$catData = Category::getPCategory($subCatSlug);
-			$this->getCategory($catData->id, $subCatSlug->id);
-
-			// Get Category nested IDs
-			$catNestedIds = (object)[
-				'parentId' => $catData->id,
-				'id'       => $subCatSlug->id,
-			];
+		if (request()->filled('c')) {
+			$catId = Category::getFieldId(request()->get('c'));
+			if (request()->filled('sc')) {
+				$subCatId = Category::getFieldId(request()->get('sc'));
+				$this->getCategory($catId, $subCatId);
+				
+				// Get Category nested IDs
+				$catNestedIds = (object)[
+					'parentId' => $catId,
+					'id'       => $subCatId,
+				];
+			} else {
+				$this->getCategory($catId);
+				
+				// Get Category nested IDs
+				$catNestedIds = (object)[
+					'parentId' => 0,
+					'id'       => $catId,
+				];
+			}
+			
+			// Get Custom Fields
+			$customFields = CategoryField::getFields($catNestedIds);
+			view()->share('customFields', $customFields);
 		}
-		
-		// Get Custom Fields
-		$customFields = CategoryField::getFields($catNestedIds);
-		view()->share('customFields', $customFields);
-		if ($locationID) {
-			$city = $this->getCity($locationID);
+		if (request()->filled('l') || request()->filled('location')) {
+			$city = $this->getCity(request()->get('l'), request()->get('location'));
+		}
+		if (request()->filled('r') && !request()->filled('l')) {
+			$admin = $this->getAdmin(request()->get('r'));
 		}
 		
 		// Pre-Search values
@@ -88,7 +93,7 @@ class SearchController extends BaseController
 		// Meta Tags
 		MetaTag::set('title', $title);
 		MetaTag::set('description', $title);
-
+		
 		return view('search.serp');
 	}
 }
