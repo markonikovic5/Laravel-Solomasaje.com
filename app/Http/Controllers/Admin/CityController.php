3ac6f89f9acf5d30ad682fbb8ad301108c533c15
cydoa11 <light.oa17@gmail.com>
2020-11-04 11:55:40 +0100

    301

--- app/Http/Controllers/Admin/CityController.php
+++ app/Http/Controllers/Admin/CityController.php
@@ -39,7 +39,7 @@ class CityController extends PanelController
 		// Get the parent Entity slug
 		$this->parentEntity = request()->segment(2);
 		if (!in_array($this->parentEntity, $parentEntities)) {
-			abort(404);
+			abort(301);
 		}
 		
 		// Country => City
