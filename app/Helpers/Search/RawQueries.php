d23389046ce80709ea4294b9764b2078d005cc12
cydoa11 <light.oa17@gmail.com>
2020-11-04 14:55:23 +0100

    rmv

--- app/Helpers/Search/RawQueries.php
+++ app/Helpers/Search/RawQueries.php
@@ -302,7 +302,6 @@ class RawQueries
 		// Count all entries
 		$sql = "SELECT COUNT(*) AS total FROM (" . $sql . ") AS x";
 		$all = self::execute($sql, $this->bindings);
-		dd ($this->bindings);
 		$count['all'] = (isset($all[0])) ? $all[0]->total : 0;
 		
 		// Get the Post's Types
